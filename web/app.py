7a4d20aeb58a64051d5da5cf07a508d8812679af
Cole Sabin <cskozmo@gmail.com>
2018-01-26 16:17:18 -0800

    Updated app to serve HTML

--- web/app.py
+++ web/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, make_response
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<name>')
+def pages(name):
+    if '..' in name or '//' in name or '~' in name:
+            response = make_response(render_template('403.html'), 403)
+            return response
+    return redirect(url_for('static', filename=name))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    response = make_response(render_template('404.html'), 404)
+    return response
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+     app.run(debug=True,host='0.0.0.0')
